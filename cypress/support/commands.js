814254938fbdcda0a3db51d8de1764074bcb05e5
comfortowo <comfortowo4@gmail.com>
2026-02-13 13:07:39 +0100

    feat: complete assessment including manual, exploratory, and automated tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -14,14 +14,19 @@ Cypress.Commands.add('checkLoginAndPause', () => {
   // 3. Check URL
   cy.url().then((url) => {
     if (url.includes('login') || url.includes('signin')) {
-      
+      cy.get('[name="email"]').type('qa@coreloops.ai');
+      cy.get('.inline-flex').click();
+      cy.wait(3000);
+    
+      cy.get('.gap-2').click();
       // --- THE PAUSE MESSAGE ---
-      cy.log(' MANUAL LOGIN REQUIRED ');
-      cy.log('Please enter Email & Code manually.');
-      cy.log('Click "Resume" (Play) above when finished.');
+ 
+      // cy.log(' MANUAL LOGIN REQUIRED ');
+      // cy.log('Please enter Email & Code manually.');
+      // cy.log('Click "Resume" (Play) above when finished.');
       
       // 4. Pause for User Input
-      cy.pause(); 
+      // cy.pause(); 
     } else {
       cy.log('User is already logged in!');
     }
